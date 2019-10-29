dd69702081f0da433f5203c8f9252998df68e40b
CADIOU <lp.cadiou1996@gmail.com>
2019-10-29 16:30:52 +0100

    classe Forme_bordure

--- src/sample/Forme_Bordure.java
+++ src/sample/Forme_Bordure.java
@@ -14,30 +14,41 @@ public class Forme_Bordure extends Shape {
     ArrayList<Circle> liste_cercle_controle ;
     ArrayList<CubicCurveTo> liste_cubicCurveTo ;
     ArrayList<MoveTo> liste_Moveto ;
+    LineTo liste_plate ;
+
+    Boolean est_plat ;
 
     Path notre_path ;
 
-    Forme_Bordure(){
-        // Création de notre liste de cercle
-        liste_cercle = new ArrayList<>();
-        for (int i = 0; i < NB_CERCLE_PAR_BORDURE ; i++) { // 7 cercles
-            liste_cercle.add(new Circle(10 ,Color.RED));
+    Forme_Bordure(Boolean est_plat){
+        if ( !est_plat ) { // coté avec creux et ou dents
+            // Création de notre liste de cercle
+            liste_cercle = new ArrayList<>();
+            for (int i = 0; i < NB_CERCLE_PAR_BORDURE; i++) { // 7 cercles
+                liste_cercle.add(new Circle(10, Color.RED));
+            }
+            // Création de notre liste de cercle de controle
+            liste_cercle_controle = new ArrayList<>();
+            for (int i = 0; i < (NB_CERCLE_PAR_BORDURE - 1) * 2; i += 2) { // 12 points de controles
+                liste_cercle.add(new Circle(10, Color.BLUE)); // premier point de controle en bleu
+                liste_cercle.add(new Circle(10, Color.GREEN)); // deuxieme en vert
+            }
+            // creation de notre liste de cubicCurveTo
+            liste_cubicCurveTo = new ArrayList<>();
+            for (int i = 0; i < NB_CERCLE_PAR_BORDURE - 1; i++) { // 6 Curves
+                liste_cubicCurveTo.add(new CubicCurveTo());
+            }
+            liste_Moveto = new ArrayList<>();
+            for (int i = 0; i < NB_CERCLE_PAR_BORDURE - 1; i++) { // 6 MoveTo pour aller avec les 6 Curves
+                liste_Moveto.add(new MoveTo());
+            }
+            notre_path = new Path();
         }
-        // Création de notre liste de cercle de controle
-        liste_cercle_controle = new ArrayList<>();
-        for (int i = 0; i < ( NB_CERCLE_PAR_BORDURE-1 ) * 2 ; i+=2) { // 12 points de controles
-            liste_cercle.add(new Circle(10 ,Color.BLUE)); // premier point de controle en bleu
-            liste_cercle.add(new Circle(10 ,Color.GREEN)); // deuxieme en vert
+        else {
+            liste_cercle = new ArrayList<>();
+            liste_cercle.add( new Circle(10,Color.GOLD) ) ;
+            liste_cercle.add(new Circle(10 ,Color.GOLD) ) ;
+            liste_plate = new LineTo();
         }
-        // creation de notre liste de cubicCurveTo
-        liste_cubicCurveTo = new ArrayList<>();
-        for (int i = 0; i < NB_CERCLE_PAR_BORDURE-1 ; i++) { // 6 Curves
-            liste_cubicCurveTo.add(new CubicCurveTo());
-        }
-        liste_Moveto = new ArrayList<>();
-        for (int i = 0; i < NB_CERCLE_PAR_BORDURE-1 ; i++) { // 6 MoveTo pour aller avec les 6 Curves
-            liste_Moveto.add(new MoveTo());
-        }
-        notre_path = new Path();
     }
 }
