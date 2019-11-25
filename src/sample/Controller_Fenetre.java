155d481c19e8892c4e3f285e026b3a6adb5bd343
TRAORE paul <traore@informatique.univ-paris-diderot.fr>
2019-11-25 23:53:42 +0100

    fonction reset et validation du parametrage debut fonctions de creation de plateau pour le plateau d'assemblage

--- src/sample/Controller_Fenetre.java
+++ src/sample/Controller_Fenetre.java
@@ -9,24 +9,35 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
 import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.shape.Circle;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+import java.awt.*;
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 
 public class Controller_Fenetre  {
-    private static int width_win = 600;
-    private static int height_win = 600;
+    private static int width_plateau = 800;
+    private static int height_plateau = 500;
     private static final int DEFAULT_NIVEAU = 0;
     private Stage stage;
     private Scene scene;
     private Image image;
+
+    private Plateau plateau;
+
     private static final int min_longueur = Piece.MIN_LONGUEUR;
     private static final int min_hauteur = Piece.MIN_HAUTEUR;
     private int niveau=DEFAULT_NIVEAU;
@@ -54,6 +65,24 @@ public class Controller_Fenetre  {
         this.stage = Main.getPrimary_Stage();
     }
 
+
+    private void create_Plateau() {
+        this.plateau = new Plateau(Integer.valueOf(nb_ligne.getText().toString()),
+                                Integer.valueOf(nb_colonne.getText().toString()),
+                                width_plateau,
+                                height_plateau);
+    }
+
+    private void set_plateau_on_pane() {
+        Pane pane = new Pane();
+        /*
+        for (int j = 0; j < plateau.getNb_colonne(); j++) {
+            for (int i = 0; i < plateau.getNb_ligne() ; i++) {
+                root.getChildren().add(plateau.getTab()[i][j].forme);
+            }
+        }
+        */
+    }
     @FXML
     private void fill_new() throws IOException {
         System.out.println("Dans fill_new");
@@ -83,6 +112,16 @@ public class Controller_Fenetre  {
     }
 
     @FXML
+    private void fill_enonce() throws IOException {
+        // TODO : gerer les erreur affiche dans le terminal lors de l'execution de cette fonction
+        Desktop d = Desktop.getDesktop();
+        d.open(new File("./enonce/puzzle.pdf"));
+    }
+    @FXML
+    private void fill_wiki_puzzle() throws URISyntaxException, IOException {
+        Desktop.getDesktop().browse(new URI("https://fr.wikipedia.org/wiki/Puzzle"));
+    }
+    @FXML
     private void fill_quit() {
         quit.setOnAction(e -> Platform.exit());
         // Fermeture propre de l'application
@@ -109,20 +148,40 @@ public class Controller_Fenetre  {
 
     @FXML
     private void reset_onAction() {
-
+        this.image = null;
+        this.path_image.setText("");
+        set_element_hide();
+        this.spinner_ligne.setValueFactory(null);
+        this.spinner_colonne.setValueFactory(null);
     }
 
     @FXML
-    private void valider_onAction() {
-
+    private void valider_onAction() throws IOException {
+        System.out.println("dans valider on action");
+        if (check_parametrage()) {
+            //TODO : retour a la fenetre principale et debut du jeu
+            final URL url = getClass().getResource("Fenetre.fxml");
+            // Création du loader
+            System.out.println("1");
+            final FXMLLoader fxmlLoader = new FXMLLoader(url);
+            System.out.println("2");
+            // Chargement du FXML
+            final BorderPane root = fxmlLoader.load();
+            System.out.println("3");
+            // Création de la scène
+            if (root != null) {
+                System.out.println("root n'est pas null");
+                this.stage.setScene(new Scene(root, 800, 600));
+            }
+        }
     }
-    /*
+
     //verifie si tout les champs du paarametrage dd'un nouveau puzzle sont bien remplie
     private boolean check_parametrage() {
         boolean res = false;
         if (check_image()) {
             if (niveau != 0) {
-
+                res = true;
             }
         }
         return  res;
@@ -134,7 +193,7 @@ public class Controller_Fenetre  {
         }
         return false;
     }
-*/
+
     @FXML
     private void chooseImage() {
         event1();
@@ -143,8 +202,22 @@ public class Controller_Fenetre  {
             init_spinner();
         }
     }
+
+    private void set_element_hide() {
+        this.nb_ligne.setVisible(false);
+        this.nb_colonne.setVisible(false);
+        this.spinner_colonne.setVisible(false);
+        this.spinner_ligne.setVisible(false);
+    }
+    private void set_element_Visible() {
+        this.nb_ligne.setVisible(true);
+        this.nb_colonne.setVisible(true);
+        this.spinner_colonne.setVisible(true);
+        this.spinner_ligne.setVisible(true);
+    }
     // init les spinner en fonction de la resolution de l'image choisi
     private void init_spinner() {
+        set_element_Visible();
         int h = (int) image.getHeight();
         int max_li = (int) (h / min_hauteur);// pas besoin des max ??????????
         int w = (int) image.getWidth();
